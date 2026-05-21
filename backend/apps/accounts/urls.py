66590b3e61faa62baf088bf56bb36b693c53e16e
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-05-21 14:48:57 +0330

    feat(accounts): add TokenRefreshView and TokenVerifyView URLs

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
-    path("")
+    path("api/token/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify")
 ]
