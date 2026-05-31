1c7a84cc648f3894985a6a1f8a0079fffee2f3b2
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-05-31 17:00:33 +0330

    feat(db): create ComplexManagerRequest table with indexes

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserAccount, Profile
+from .models import UserAccount, Profile, ComplexManagerRequest
 
 
 admin.site.register(UserAccount)
 admin.site.register(Profile)
+admin.site.register(ComplexManagerRequest)
