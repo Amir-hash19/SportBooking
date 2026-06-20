5e0504088f984bf9011d76c3c62e86d73ebde424
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-06-20 14:57:51 +0330

    bugfix: install whitenoise to collect static files

--- backend/settings.py
+++ backend/settings.py
@@ -68,6 +68,10 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     
     "django.middleware.security.SecurityMiddleware",
+
+    #whitenoise middleware
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -155,6 +159,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
