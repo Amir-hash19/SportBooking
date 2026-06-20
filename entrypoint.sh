c311b6123236639d864c4cd4a740aad378adcc27
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-06-20 16:53:52 +0330

    bug:-changed the gunicorn port in entrypoint filed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
